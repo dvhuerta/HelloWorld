6ad1347b527314fa8bfa093f347d2cff105635f9
dvhuerta <diegovhh7ayc@hotmail.es>
2017-10-25 16:26:54 -0500

    The project of chickens with new functions

--- Chicken.java
+++ Chicken.java
@@ -3,16 +3,25 @@ public class Chicken{
 	private String name;
 	private int age;
 	public void layAnEgg(){
-		System.out.println("The chicken lay an egg");
+		System.out.println("(>) The chicken lay an egg");
+	}
+	public void negationLayAnEgg(){
+		System.out.println("(x) The chicken don't lay an egg");
 	}
 	public void poop(){
-		System.out.println("The chicken pooped");
+		System.out.println("(>) The chicken pooped");
+	}
+	public void negationPoop(){
+		System.out.println("(x) The chicken don't  do poop");
 	}
 	public void drink(){
-		System.out.println("The chicken drank");
+		System.out.println("(>) The chicken drank");
+	}
+	public void negationDrink(){
+		System.out.println("(x) The chicken doesn't drink");
 	}
 	public void print(){
-		System.out.println("The name of the chicken is: "+name+", color: "+color+", age: "+age+" ");
+		System.out.println("*The name of the chicken is: "+name+", color: "+color+", age: "+age+" ");
 	}
 	public void initializeData(String name, String color, int age){
 		this.name=name;
