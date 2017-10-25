86c375e7f7ebb1728761948de57af55ee2ff3320
dvhuerta <diegovhh7ayc@hotmail.es>
2017-10-25 11:11:19 -0500

    HelloWorld with the chicken

--- HelloWord.java
+++ HelloWord.java
@@ -1,7 +1,10 @@
-public class HelloWorld {
+public class HelloWord {
 
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+		Chicken chicken;
+	chicken = new Chicken();
+	chicken.initializeData("Lucy","coffe",2);
+	chicken.print();
     }
-    
 }
