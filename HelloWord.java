edca54238f9a2473215e26f817c8c1c0018d6a31
dvhuerta <diegovhh7ayc@hotmail.es>
2017-10-25 16:23:41 -0500

    Project with four chickens

--- HelloWord.java
+++ HelloWord.java
@@ -1,10 +1,42 @@
 public class HelloWord {
-
-    public static void main(String[] args) {
-        System.out.println("HelloWorld");
-		Chicken chicken;
-	chicken = new Chicken();
-	chicken.initializeData("Lucy","coffe",2);
-	chicken.print();
-    }
+    public static void main(String[] argsOne) {
+		System.out.println("");
+		System.out.println("***HelloWorld***");
+		System.out.println("");
+		System.out.println(" -First Chicken");
+        Chicken chickenOne;
+	chickenOne = new Chicken();
+	chickenOne.initializeData("Lucy","coffe",2);
+	chickenOne.print();
+	chickenOne.layAnEgg();
+	chickenOne.drink();
+	chickenOne.poop();
+		System.out.println("");
+		System.out.println(" -Second Chicken");
+		Chicken chickenTwo;
+	chickenTwo = new Chicken();
+	chickenTwo.initializeData("Cuqui","white",1);
+	chickenTwo.print();
+	chickenTwo.layAnEgg();
+	chickenTwo.negationDrink();
+	chickenTwo.negationPoop();
+		System.out.println("");
+		System.out.println(" -Third Chicken");
+		Chicken chickenThree;
+	chickenThree = new Chicken();
+	chickenThree.initializeData("Aby","black",4);
+	chickenThree.print();
+	chickenThree.negationLayAnEgg();
+	chickenThree.drink();
+	chickenThree.negationPoop();
+		System.out.println("");
+		System.out.println(" -Fourth Chicken");
+		Chicken chickenFour;
+	chickenFour = new Chicken();
+	chickenFour.initializeData("Paty","yellow",3);
+	chickenFour.print();
+	chickenFour.negationLayAnEgg();
+	chickenFour.negationDrink();
+	chickenFour.poop();	
+	}
 }
